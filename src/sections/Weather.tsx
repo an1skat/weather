65b54004c6dccaba742a900d51976dc5cf51305a
an1skat <antskat0@gmail.com>
2025-11-15 09:56:23 +0200

    add some sections file

--- src/sections/Weather.tsx
+++ src/sections/Weather.tsx
@@ -7,21 +7,24 @@ import "swiper/css/navigation";
 
 export default function Weather() {
 	return (
-		<Swiper
-			slidesPerView={1}
-			modules={[Navigation]}
-			navigation
-			loop
-		>
-			<SwiperSlide>
-				<WeatherCard city="Prague" country="Czech Republic" weather="clear sky" temp={22} />
-			</SwiperSlide>
-			<SwiperSlide>
-				<WeatherCard city="Kiiv" country="Czech Republic" weather="fog" temp={13} />
-			</SwiperSlide>
-			<SwiperSlide>
-				<WeatherCard city="Prague" country="Moldova" weather="light rain" temp={5} />
-			</SwiperSlide>
-		</Swiper>
+        <section className="weather">
+                <Swiper
+                    spaceBetween={50}
+                    slidesPerView={1}
+                    modules={[Navigation]}
+                    navigation
+                    loop
+                >
+                    <SwiperSlide>
+                        <WeatherCard city="Prague" country="Czech Republic" weather="clear sky" temp={22} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <WeatherCard city="Kiiv" country="Czech Republic" weather="fog" temp={13} />
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <WeatherCard city="Prague" country="Moldova" weather="light rain" temp={5} />
+                    </SwiperSlide>
+                </Swiper>
+        </section>
 	)
 }
